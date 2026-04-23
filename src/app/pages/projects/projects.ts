b7261d7630acd2d144bedd252afbcb4882a14a9e
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2026-04-23 17:22:11 +0400

    dot for img

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -13,7 +13,7 @@ export class Projects {
   {
     title: 'LexSport Georgia - Sports Law Platform',
     description: 'Full-stack app with Angular',
-    image: '/LexSportScreen.png',
+    image: './LexSportScreen.png',
     live: 'https://lexsportgeorgia.uk/',
   }
 ];
